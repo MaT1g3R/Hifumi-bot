1d09e8e69d994bef1dd1faa33973cd2a151c2a29
Umi <982341777@qq.com>
2017-05-01 16:15:27 -0400

    help coming soon tm

--- cogs/bot_info.py
+++ cogs/bot_info.py
@@ -1,12 +1,15 @@
 """
 Cog for bot info
 """
+import time
+
 from discord.ext import commands
+
+from config.settings import INVITE
 from core.bot_info_core import build_info_embed, handle_support
 
 
 class BotInfo:
-
     def __init__(self, bot):
         """
         Initialized the BotInfo class
@@ -31,20 +34,46 @@ class BotInfo:
 
     @commands.command()
     async def donate(self):
-        pass
+        await self.bot.say("Thanks for your generosity! "
+                           "Hifumi#8451 and Rem#5307 are alive thanks to "
+                           "your help and our dearest contributor (Wolke) help."
+                           " If you want to donate, please consider going to "
+                           "the following link: "
+                           "<http://www.hifumibot.xyz/donate>. "
+                           "Once you donate, make sure to DM "
+                           "Underforest#1284 with a proof of your donation, "
+                           "so you can get a special bonus for you. "
+                           "This includes a personal \"thank you\" from Wolke "
+                           "and Underforest, Donators role for Hifumi and Rem "
+                           "support servers, early beta testing for both bots, "
+                           "special Hifumi features for "
+                           "donators and so much more.")
 
     @commands.command()
     async def git(self):
-        pass
+        await self.bot.say(
+            'Open source can be found here: '
+            '<https://github.com/hifumibot/hifumibot>\n'
+            'As well as the documentation: <http://hifumibot.xyz/docs>'
+        )
 
     @commands.command()
     async def help(self):
-        pass
+        await self.bot.say('Coming Soon')
 
     @commands.command()
     async def ping(self):
-        pass
+        start_time = int(round(time.time() * 1000))
+        msg = await self.bot.say('Pong! :hourglass:')
+        end_time = int(round(time.time() * 1000))
+        await self.bot.edit_message(
+            msg, 'Pong! | :timer: {}ms'.format(end_time - start_time))
 
     @commands.command()
     async def invite(self):
-        pass
+        await self.bot.say(
+            "Use this link to invite me to your server:\n"
+            "**NOTE: Select all permissions as needed but it's"
+            " recommendable to keep enabled all, so I can work fine.**\n"
+            "<{}>".format(INVITE)
+        )
