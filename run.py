3a2c3b5c6d1caa73468aab4ae4b6fc904572ed2e
Umi <982341777@qq.com>
2017-05-04 01:12:56 -0400

    updated bot run file

--- run.py
+++ run.py
@@ -4,14 +4,17 @@ The main run file.
 from os.path import join
 
 from cogs import bot_info, testing, channel_reader, nsfw
-from config.settings import TOKEN, SHARD_COUNT, SHARD_ID
+from config.settings import TOKEN, SHARD_COUNT, SHARD_ID, SHARDED
 from core.data_controller import DataController
 from core.discord_functions import get_prefix
 from shell.hifumi import Hifumi
 
 if __name__ == '__main__':
     d = DataController(join('data', 'hifumi_db'))
-    bot = Hifumi(get_prefix, d, shard_count=SHARD_COUNT, shard_id=SHARD_ID)
+    if SHARDED:
+        bot = Hifumi(get_prefix, d, shard_count=SHARD_COUNT, shard_id=SHARD_ID)
+    else:
+        bot = Hifumi(get_prefix, d)
     cogs = [bot_info.BotInfo(bot), testing.Testing(bot),
             channel_reader.ChannelReader(bot), nsfw.Nsfw(bot)]
 
