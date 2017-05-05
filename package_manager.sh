bb688eee216495e16a862b2cebd51ec2c0f8e249
Underforest <neovisatoons@gmail.com>
2017-05-05 15:55:02 -0700

    Update package_manager.sh

--- package_manager.sh
+++ package_manager.sh
@@ -4,4 +4,11 @@
 # Please don't attempt to edit this script to avoid further
 # problems with your environment.
 
-echo "Work in progress!"
+echo " __    __   __   _______  __    __   ___  ___   __"
+echo "|  |  |  | |  | |   ____||  |  |  | |   \/   | |  |    _"
+echo "|  |__|  | |  | |  |__   |  |  |  | |  \  /  | |  |  _| |_"
+echo "|   __   | |  | |   __|  |  |  |  | |  |\/|  | |  | |_   _|"
+echo "|  |  |  | |  | |  |     |  `--'  | |  |  |  | |  |   |_|"
+echo "|__|  |__| |__| |__|      \______/  |__|  |__| |__|"
+echo ""
+echo "SoonTM"
