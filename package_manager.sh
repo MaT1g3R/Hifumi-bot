e17cdebfb629e8496a0a84d0d691330f306ac2f0
Underforest <neovisatoons@gmail.com>
2017-05-05 13:49:12 -0700

    Create package_manager.sh

---
+++ package_manager.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script manages the Linux packages that are needed for
+# Hifumi. Should work for all Linux distributions as well.
+# Please don't attempt to edit this script to avoid further
+# problems with your environment.
+
+echo "Work in progress!"
