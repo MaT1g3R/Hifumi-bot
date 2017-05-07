d5ae8191ff746a317e2d32ac85407ca824b15001
Umi <982341777@qq.com>
2017-05-07 03:56:38 -0400

    updated autoclean

--- autoclean.py
+++ autoclean.py
@@ -1,4 +1,22 @@
+import os
 import shutil
+
+
+def __autoclean(path=(os.getcwd())):
+    """
+    Cleans automatically Python cache.
+    :return: Clean if successful.
+    """
+    if os.path.isdir(path) and '__pycache__' in path:
+        return [path]
+    elif os.path.isfile(path):
+        return []
+    else:
+        res = []
+        for file_name in os.listdir(path):
+            subitem = os.path.join(path, file_name)
+            res += __autoclean(subitem)
+        return res
 
 
 def autoclean():
@@ -6,7 +24,9 @@ def autoclean():
     Cleans automatically Python cache.
     :return: Clean if successful.
     """
-    try:
-        shutil.rmtree('./__pycache__')
-    except FileNotFoundError:
-        pass
+    paths = __autoclean()
+    for path in paths:
+        try:
+            shutil.rmtree(path)
+        except FileNotFoundError:
+            pass
