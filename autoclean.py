4e2d573d02d49f99cda344de8ecd5bd8e8211ff0
Underforest <neovisatoons@gmail.com>
2017-05-06 19:49:59 -0700

    Create autoclean.py

---
+++ autoclean.py
@@ -0,0 +1,11 @@
+import shutil
+
+def autoclean():
+    """
+    Cleans automatically Python cache.
+    :return: Clean if successful.
+    """
+    try:
+        shutil.rmtree('./__pycache__')
+    except FileNotFoundError:
+        pass
