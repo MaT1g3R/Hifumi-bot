d2473c5bef4c49614ea95d4026ee0abada37d490
Umi <982341777@qq.com>
2017-05-10 19:37:47 -0400

    Update autoclean to delete logs

--- autoclean.py
+++ autoclean.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 
-def __autoclean(path=(os.getcwd())):
+def __find_cache(path=(os.getcwd())):
     """
     Helper function to get all "__pycache__" folders
     :return: A list of paths with name __pycache__
@@ -15,31 +15,37 @@ def __autoclean(path=(os.getcwd())):
         res = []
         for file_name in os.listdir(path):
             subitem = os.path.join(path, file_name)
-            res += __autoclean(subitem)
+            res += __find_cache(subitem)
         return res
+
+
+def __autoclean(path, filter_):
+    """
+    A helper function to delete all files under a folder
+    :param path: the path that points to the folder
+    :param filter_: the filter keyword will prevent deleting any file with the
+    word in its name
+    """
+    for file in os.listdir(path):
+        if filter_ not in file:
+            try:
+                os.remove(os.path.join(path, file))
+            except OSError:
+                continue
 
 
 def autoclean():
     """
     Cleans all Python cache.
     """
-    for path in __autoclean():
+    for path in __find_cache():
         shutil.rmtree(path, ignore_errors=True)
     dumps = os.path.join('data', 'dumps')
     shard_info = os.path.join('data', 'shard_info')
+    logs = os.path.join('data', 'logs')
     dummy = 'dummy.txt'
-    for file in os.listdir(dumps):
-        if dummy not in file:
-            try:
-                os.remove(os.path.join(dumps, file))
-            except OSError:
-                continue
-    for file in os.listdir(shard_info):
-        if dummy not in file:
-            try:
-                os.remove(os.path.join(shard_info, file))
-            except OSError:
-                continue
+    for path in [dumps, shard_info, logs]:
+        __autoclean(path, dummy)
 
 
 if __name__ == '__main__':
