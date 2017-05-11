ca8c138f1f2d32b35c6ad4a7f1c54a2d21794af3
Umi <982341777@qq.com>
2017-05-11 02:08:18 -0400

    updated suite.py for command line support

--- tests/suite.py
+++ tests/suite.py
@@ -1,5 +1,6 @@
 import unittest
+from pathlib import Path
 
 if __name__ == '__main__':
-    testsuite = unittest.TestLoader().discover('.')
+    testsuite = unittest.TestLoader().discover('.', top_level_dir=Path('..'))
     unittest.TextTestRunner(verbosity=2).run(testsuite)
