7b9900fefdb5921e2b46031fd907959cfb01b988
Underforest <neovisatoons@gmail.com>
2017-06-02 23:51:42 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+install_reqs = parse_requirements("./config/requirements.txt")
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='hifumi',
+    version='0.0.1',
+    packages=find_packages()
+	  install_requires=reqs,
+	  include_package_data=True,
+    url='http://www.hifumibot.xyz',
+    license='GPL-3.0',
+    author='Underforest',
+    author_email='neovisatoons@gmail.com',
+    description='The official module for Hifumi, the Discord bot.',
+	long_description=read('README.md')
+	)
