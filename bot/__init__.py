573f8f55b77c7a7d3b21d41732ff9988ce04a7d8
Umi <982341777@qq.com>
2017-06-19 20:54:14 -0400

    version bump to 0.1.1

--- bot/__init__.py
+++ bot/__init__.py
@@ -19,12 +19,12 @@ __helper__ = ['Wolke#6746']
 __helper_plain__ = ['DasWolke']
 __license__ = 'GNU General Public License v3.0'
 __copyright__ = 'Copyright 2017 Hifumi - the Discord Bot Project'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
 version_info = VersionInfo(
-    major=0, minor=1, micro=0, releaselevel='alpha', serial=0
+    major=0, minor=1, micro=1, releaselevel='alpha', serial=0
 )
 
 __all__ = ['__title__', '__author__', '__author_plain__', '__helper__',
