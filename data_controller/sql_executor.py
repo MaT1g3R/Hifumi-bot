739847689152d540f726651a114caf97fd0243b7
Umi <982341777@qq.com>
2017-06-21 03:54:13 -0400

    Migrate to Postgres

--- data_controller/sql_executor.py
+++ data_controller/sql_executor.py
@@ -1,36 +1,153 @@
-"""
-A sqlite3 database controller, this is not meant to be accessed directly with
-the bot. For bot use, please use the DataManager class.
-"""
-from sqlite3 import Connection, Cursor
+from datetime import time
 from typing import Dict, List, Sequence
 
-from scripts.helpers import assert_inputs, assert_outputs
+from asyncpg import Connection, connect
+from asyncpg.cursor import Cursor
+from asyncpg.prepared_stmt import PreparedStatement
+
+from scripts.helpers import assert_types
 
 # Types expected for the different tables
-__guild_types = (int, str, str, int, str)
-__member_types = (int, int, int)
-__user_types = (int, int, int)
-__tag_types = (str, str)
-
-__all__ = ['_get_guild_row', '_get_member_row', '_get_tags', '_get_user_row',
-           '_write_guild_row', '_write_member_row', '_write_tags',
-           '_write_user_row']
+_guild_types = (int, str, str, int, list)
+_member_types = (int, int, int)
+_user_types = (int, int, time)
+_tag_types = (str, str)
 
 
-@assert_outputs(__guild_types, True)
-def _get_guild_row(cursor: Cursor, guild_id: int) -> tuple:
+class Postgres:
+    """
+    A Postgres database controller, this is not meant to be accessed directly with
+    the bot. For bot use, please use the DataManager class.
+    """
+
+    def __init__(self, conn: Connection):
+        """
+        Initialize the instance of this class.
+        :param conn: the asyncpg connection.
+        """
+        self.__ready = False
+        self.__conn: Connection = conn
+
+        self.__get_guild: PreparedStatement = None
+        self.__set_guild: PreparedStatement = None
+        self.__get_member: PreparedStatement = None
+        self.__set_member: PreparedStatement = None
+        self.__get_user: PreparedStatement = None
+        self.__set_user: PreparedStatement = None
+        self.__get_tags: PreparedStatement = None
+        self.__set_tags: PreparedStatement = None
+
+    async def prepare(self, schema: str):
+        """
+        Prepare the queries that will be used.
+        :param schema: the Postgres schema name.
+        """
+        f = self.__conn.prepare
+
+        self.__get_guild = await f(
+            'SELECT * FROM {}.guild_info WHERE guild_id = $1'.format(schema)
+        )
+
+        self.__set_guild = await f(
+            'INSERT INTO {}.guild_info VALUES ($1, $2, $3, $4, $5) '
+            'ON CONFLICT (guild_id) '
+            'DO UPDATE SET prefix=$2, lan=$3, mod_log=$4, roles=$5'.format(
+                schema)
+        )
+
+        self.__get_member = await f(
+            'SELECT * FROM {}.member_info '
+            'WHERE member_id=$1 AND guild_id=$2'.format(schema)
+        )
+
+        self.__set_member = await f(
+            'INSERT INTO {}.member_info VALUES ($1, $2, $3) '
+            'ON CONFLICT (member_id, guild_id)'
+            'DO UPDATE SET warns=$3'.format(schema)
+        )
+
+        self.__get_user = await f(
+            'SELECT * FROM {}.user_info WHERE user_id=$1'.format(schema)
+        )
+
+        self.__set_user = await f(
+            'INSERT INTO {}.user_info VALUES ($1, $2, $3) '
+            'ON CONFLICT (user_id) '
+            'DO UPDATE SET balance=$2, daily=$3'.format(schema)
+        )
+
+        self.__get_tags = await f('SELECT * FROM {}.nsfw_tags'.format(schema))
+
+        self.__set_tags = await f(
+            'INSERT INTO {}.nsfw_tags VALUES ($1, $2) '
+            'ON CONFLICT (site, tag_name) '
+            'DO NOTHING'.format(schema)
+        )
+        self.__ready = True
+
+    async def get_guild(self, guild_id: int) -> tuple:
+        """
+        Get guild row by id.
+        :param guild_id: the guild id.
+        :return: the guild row.
+        """
+        assert self.__ready
+        res = (await self.__get_guild.fetchrow(guild_id) or
+               (None,) * len(_guild_types))
+        assert_types(res, _guild_types, True)
+        return res
+
+    async def set_guild(self, values: Sequence):
+        """
+        Set a guild row.
+        :param values: the values of that row.
+        """
+        assert self.__ready
+        assert_types(values, _guild_types, True)
+        await self.__set_guild.fetch(*values)
+
+    async def get_member(self, member_id: int, guild_id: int) -> tuple:
+        """
+        Get a member row.
+        :param member_id: the member id.
+        :param guild_id: the guild id.
+        :return: the member row.
+        """
+        assert self.__ready
+        res = (await self.__get_member.fetchrow(member_id, guild_id) or
+               (None,) * len(_member_types))
+        assert_types(res, _member_types, True)
+        return res
+
+    async def set_member(self, values: Sequence):
+        """
+        Set a member row.
+        :param values: the values of that row.
+        """
+        assert self.__ready
+        assert_types(values, _member_types, True)
+        await self.__set_member.fetch(*values)
+
+
+async def _get_guild_row(connection: Connection, guild_id: int):
     """
     Get the row with the guild_id from guild_info table.
-    :param cursor: the sqlite3 cursor object.
+    :param connection: the postgres connection
     :param guild_id: the guild id.
     :return: a tuple of the columns in that row.
     """
-    cursor.execute('SELECT * FROM guild_info WHERE guild=?', (guild_id,))
-    return cursor.fetchone() or (None,) * len(__guild_types)
+    s = 'testing'
+    p = 'production'
+    stmt = await connection.prepare(
+        'SELECT * FROM {}.guild_info WHERE guild_id = $1'.format(p))
+    res = await stmt.fetchrow(guild_id) or (None,) * len(_guild_types)
+    assert_types(res, _guild_types, True)
+    print(res)
+    # await connection.execute('SELECT * FROM guild_info WHERE guild_id=?')
+    # cursor.execute('SELECT * FROM guild_info WHERE guild=?', (guild_id,))
+    # return cursor.fetchone() or (None,) * len(_guild_types)
 
 
-@assert_inputs(__guild_types, True)
 def _write_guild_row(
         connection: Connection, cursor: Cursor, values: Sequence):
     """
@@ -43,7 +160,6 @@ def _write_guild_row(
     connection.commit()
 
 
-@assert_outputs(__member_types, True)
 def _get_member_row(cursor: Cursor, member_id: int, guild_id: int) -> tuple:
     """
     Get the row with member_id and guild_id from member_info table.
@@ -56,10 +172,9 @@ def _get_member_row(cursor: Cursor, member_id: int, guild_id: int) -> tuple:
         'SELECT * FROM member_info WHERE member=? AND guild=?',
         (member_id, guild_id)
     )
-    return cursor.fetchone() or (None,) * len(__member_types)
+    return cursor.fetchone() or (None,) * len(_member_types)
 
 
-@assert_inputs(__member_types, True)
 def _write_member_row(
         connection: Connection, cursor: Cursor, values: Sequence):
     """
@@ -72,7 +187,6 @@ def _write_member_row(
     connection.commit()
 
 
-@assert_outputs(__user_types, True)
 def _get_user_row(cursor: Cursor, user_id: int) -> tuple:
     """
     Get a row in user_info by user_id.
@@ -81,10 +195,9 @@ def _get_user_row(cursor: Cursor, user_id: int) -> tuple:
     :return: the values of that row
     """
     cursor.execute('SELECT * FROM user_info WHERE user=?', (user_id,))
-    return cursor.fetchone() or (None,) * len(__user_types)
+    return cursor.fetchone() or (None,) * len(_user_types)
 
 
-@assert_inputs(__user_types, True)
 def _write_user_row(connection: Connection, cursor: Cursor, values):
     """
     Write into user_info table.
@@ -127,3 +240,13 @@ def _write_tags(
         cursor.execute(
             'INSERT OR IGNORE INTO nsfw_tags VALUES (?, ?)', (site, tag))
     connection.commit()
+
+
+
+
+if __name__ == '__main__':
+    pass
+    # from asyncio import get_event_loop
+    #
+    # loop = get_event_loop()
+    # loop.run_until_complete(foo())
