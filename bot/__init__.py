e630945437ec0c37121cb9627d41ae37c37e6ece
Umi <982341777@qq.com>
2017-06-24 19:15:23 -0400

    Version bump to 0.1.2

--- bot/__init__.py
+++ bot/__init__.py
@@ -13,6 +13,11 @@ from collections import namedtuple
 from bot.hifumi import Hifumi
 from bot.session_manager import SessionManager
 
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+
+version_info = VersionInfo(
+    major=0, minor=1, micro=2, releaselevel='alpha', serial=0
+)
 __title__ = 'Hifumi'
 __author__ = ['Underforest#1284', 'InternalLight#9391', 'ラブアローシュート#6728']
 __author_plain__ = ['Underforest', 'InternalLight', 'MaT1g3R']
@@ -20,13 +25,7 @@ __helper__ = ['Wolke#6746']
 __helper_plain__ = ['DasWolke']
 __license__ = 'GNU General Public License v3.0'
 __copyright__ = 'Copyright 2017 Hifumi - the Discord Bot Project'
-__version__ = '0.1.1'
-
-VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
-
-version_info = VersionInfo(
-    major=0, minor=1, micro=1, releaselevel='alpha', serial=1
-)
+__version__ = '.'.join([str(i) for i in list(version_info)[:3]])
 
 __all__ = ['__title__', '__author__', '__author_plain__', '__helper__',
            '__helper_plain__', '__license__', '__copyright__', '__version__',
