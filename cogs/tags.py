67cb8e5100527ca0c78f7fcf3f3110845b70ddbf
Underforest <neovisatoons@gmail.com>
2017-06-25 19:46:27 -0700

    Empty commands for Tags

---
+++ cogs/tags.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+
+class Tags:
+    """
+    Tags cog
+    """
+    __slots__ = ['bot']
+
+    def __init__(self, bot):
+        """
+        Initialize the Tags class
+        :param bot: the discord bot object
+        """
+        self.bot = bot
+    
+    @commands.command()
+    async def tag():
+        raise NotImplementedError
