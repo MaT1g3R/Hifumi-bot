5ee3539b64138118c54cfdbb6c036e8fc6a95047
Umi <982341777@qq.com>
2017-08-11 00:34:46 -0400

    Update configs

--- config/config.py
+++ config/config.py
@@ -1,55 +1,33 @@
 from pathlib import Path
 
-from ruamel.yaml import YAML
-from ruamel.yaml.comments import CommentedMap
-from ruamel.yaml.scalarstring import DoubleQuotedScalarString
+from yaml import safe_dump, safe_load
 
 
-class Config:
+class Config(dict):
     """
     Class to hold the config information.
     """
-    __slots__ = ['__path', '__content', '__yaml']
+    __slots__ = ['__path', '__content']
 
     def __init__(self):
         """
         Initialize the instance of this class.
         """
-        self.__yaml = YAML()
-        self.__yaml.preserve_quotes = True
         self.__path = Path(Path(__file__).parent.joinpath('settings.yml'))
         with self.__path.open(encoding='utf-8') as f:
-            self.__content = self.__yaml.load(f)
-
-    def __getitem__(self, item):
-        """
-        self.__getitem__(item) -> self[item]
-        """
-        return self.__content[item]
-
-    def __setitem__(self, key, value):
-        """ Set self[key] to value. """
-        self.__content.__setitem__(key, value)
+            self.__content = safe_load(f.read())
+        super().__init__(self.__content)
 
     def dump(self):
         """
         Dump self's contents into the config file.
         """
-        with self.__path.open('w+', encoding='utf-8') as f:
-            self.__yaml.dump(self.__content, f)
+        with self.__path.open('w', encoding='utf-8') as f:
+            safe_dump(self.__content, f)
 
-    def postgres(self) -> dict:
-        """
-        Return the content in self.__content['Postgres'] as strings.
-        :return: content in self.__content['Postgres']
-        """
+    def __setitem__(self, key, value):
+        self.__content[key] = value
+        super().__setitem__(key, value)
 
-        def convert(map_):
-            if isinstance(map_, list):
-                return [convert(s) for s in map_]
-            elif isinstance(map_, CommentedMap):
-                return {k: convert(v) for k, v in map_.items()}
-            elif isinstance(map_, DoubleQuotedScalarString):
-                return str(map_)
-
-        return convert(self.__content['Postgres'])
+    def postgres(self):
+        return self['Postgres']
